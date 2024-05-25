bb170317a8c348a7f9d6c3e9abd6458331bb7de0
nykoh2001 <nykoh2001@gmail.com>
2024-05-25 14:37:48 +0900

    [CHORE] #1 remove useless import

--- src/main/java/org/sopt/cicd/controller/ProfileController.java
+++ src/main/java/org/sopt/cicd/controller/ProfileController.java
@@ -3,7 +3,6 @@ package org.sopt.cicd.controller;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
