501344dc062d1eef436ffdf6595154c616e13039
nykoh2001 <nykoh2001@gmail.com>
2024-05-25 13:03:30 +0900

    project setting

---
+++ src/main/java/org/sopt/cicd/SoptCicdApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoptCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoptCicdApplication.class, args);
+	}
+
+}
